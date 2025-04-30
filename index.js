125ddd491637cdced229044dd093b95b2c16148f
Aditya <adityanehare571@gmail.com>
2025-05-01 03:12:53 +0530

    Added public folder, views folder and .env

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
 
-const port = 8080;
+const app = express();
+const port = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("hello World");
