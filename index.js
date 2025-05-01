39137f056092c8e1e1190ca505df685134031233
Aditya <adityanehare571@gmail.com>
2025-05-01 14:28:39 +0530

    Add EJS and EJS-MATE library

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 require("dotenv").config();
 const express = require("express");
+const engine = require("ejs-mate");
 
 const app = express();
 const port = process.env.PORT;
+
+app.engine("ejs", engine);
+app.set("views", __dirname + "/views");
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.send("hello World");
