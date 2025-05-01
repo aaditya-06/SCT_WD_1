5ec2d9886dc115d4c2b66f3fe954a74c1444b4ed
Aditya <adityanehare571@gmail.com>
2025-05-01 14:49:29 +0530

    feat: setup EJS templating with layout structure and base views

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("hello World");
+  res.render("home");
 });
 
 app.listen(port, () => {
